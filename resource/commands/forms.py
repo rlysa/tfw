08452597fe19daf7c47a29553c6a40d784d6a339
Hohrandrey <EnderBro_2005@mail.ru>
2025-04-17 20:33:56 +0300

    forms

--- resource/commands/forms.py
+++ resource/commands/forms.py
@@ -6,7 +6,6 @@ class Form(StatesGroup):
     block = State()
     main_admin = State()
     main_intern = State()
-
     registration_psw = State()
     registration_surname = State()
     registration_name = State()
@@ -16,13 +15,11 @@ class Form(StatesGroup):
     main_intern = State()
     look_interns_info = State()
     look_groups_info = State()
-
     create_task_name = State()
     create_task_interns = State()
     create_task_description = State()
     create_task_deadline = State()
     create_task_report = State()
-
     view_the_task_list = State()
     view_group_composition = State()
     view_my_info = State()
