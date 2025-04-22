08ccedf4841b3d219fd949a1a4c87c51708e0b95
rlysa <margarita.lysacheva@yandex.ru>
2025-04-22 10:22:42 +0300

    fix bag with incorrect list of usernames and ids

--- db/db_request/list_interns.py
+++ db/db_request/list_interns.py
@@ -8,8 +8,8 @@ def list_of_interns(admin):
     cursor = connection.cursor()
     interns_username = cursor.execute(f'''SELECT username FROM Interns WHERE admin="{admin}"''').fetchall()
     interns_username_str = '( "' +  '", "'.join([i[0] for i in interns_username]) + '" )'
-    interns_snm = cursor.execute(f'''SELECT surname, name, middle_name FROM Users WHERE username in {interns_username_str}''').fetchall()
-    interns = [[' '.join(interns_snm[i]), interns_username[i][0]]  for i in range(len(interns_snm))]
+    interns_snm = cursor.execute(f'''SELECT surname, name, middle_name, username FROM Users WHERE username in {interns_username_str}''').fetchall()
+    interns = [[' '.join(interns_snm[i][:-1]), interns_snm[i][-1]]  for i in range(len(interns_snm))]
     return interns
 
 
