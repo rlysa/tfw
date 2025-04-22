3ecb7be906e6a28edd60adc8b4d8eea3c32d96e3
rlysa <margarita.lysacheva@yandex.ru>
2025-04-22 10:25:10 +0300

    add new kb to look list of interns and select them

--- resource/keyboards/list_of_interns_kb.py
+++ resource/keyboards/list_of_interns_kb.py
@@ -7,3 +7,15 @@ def list_of_interns_kb(admin):
     buttons.append([InlineKeyboardButton(text='Меню команд', callback_data='back')])
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
     return keyboard
+
+
+def list_of_interns_select_kb(admin, selected):
+    buttons = []
+    for i in list_of_interns(admin):
+        if i[1] in selected:
+            buttons.append([InlineKeyboardButton(text=f'\U00002705 {i[0]}', callback_data=i[1])])
+        else:
+            buttons.append([InlineKeyboardButton(text=i[0], callback_data=i[1])])
+    buttons.append([InlineKeyboardButton(text='Далее', callback_data='next')])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
