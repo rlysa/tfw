a4518e9e972bda98c71f496f47a27282a9d2ef68
rlysa <margarita.lysacheva@yandex.ru>
2025-05-02 12:06:24 +0300

    delete intern (but dont delete users in groups)

--- resource/keyboards/change_profile_kb.py
+++
@@ -1,8 +0,0 @@
-from aiogram.utils.keyboard import KeyboardButton, ReplyKeyboardMarkup
-
-button = [[KeyboardButton(text='Изменить')],
-          [KeyboardButton(text='Меню команд')]]
-
-change_profile_keyboard = ReplyKeyboardMarkup(keyboard=button,
-                                              one_time_keyboard=True,
-                                              resize_keyboard=True)
