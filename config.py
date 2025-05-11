6d34531d55ecb7b4258b6e36eaa60645d0fc9850
Maksimka <ababva@gg.com>
2025-05-11 16:53:51 -0700

    Потому что вафля

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-TOKEN = '****'
-ADMIN_FATHER_PSW = '****'
-ADMIN_PSW = '****'
-DB_NAME = 'db/db/thw_db.db'
+import dotenv
+import os
+dotenv.load_dotenv()
+
+
+TOKEN = os.getenv('TOKEN') or '****'
+ADMIN_FATHER_PSW = os.getenv('ADMIN_FATHER_PSW') or ''
+ADMIN_PSW = os.getenv('ADMIN_PSW') or '****'
+DB_NAME = os.getenv('DB_NAME') or 'db/db/thw_db.db'
