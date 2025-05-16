e5a2e1a51adcf1687437c45fda8944aa8f531e13
rlysa <margarita.lysacheva@yandex.ru>
2025-05-16 10:29:16 +0300

    final edit (correct the work of search interns with skills)

--- db/db_request/skill_search.py
+++ db/db_request/skill_search.py
@@ -1,6 +1,16 @@
 import sqlite3
+import re
 
 from config import DB_NAME
+
+
+def enhance_search_with_regex(word, skill_query) -> list:
+    try:
+        pattern = re.compile(rf"\b{re.escape(word)}\b", flags=re.IGNORECASE)
+        skill_query = skill_query.split()
+        return [item for item in skill_query if pattern.search(item)]
+    except Exception as e:
+        return []
 
 
 def skill_search_interns(word, admin):
@@ -9,8 +19,7 @@ def skill_search_interns(word, admin):
     interns_skills = cursor.execute(f'SELECT username, skills FROM Interns WHERE admin="{admin}"').fetchall()
     result = []
     for i in interns_skills:
-        skills = i[1].split()
-        if [j for j in skills if word in j]:
+        if enhance_search_with_regex(word, i[1]):
             intern = cursor.execute(f'SELECT surname, name, middle_name FROM Users WHERE username="{i[0]}"').fetchone()
             result.append(f'{" ".join(intern)} - @{i[0]}\n{i[1]}')
     connection.close()
--- resource/commands/admin_commands/skill_search.py
+++ resource/commands/admin_commands/skill_search.py
@@ -1,7 +1,6 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-import re
 
 from ..forms import Form
 from ...keyboards.admin_keyboard import admin_keyboard
@@ -11,22 +10,13 @@ from db.db_request.skill_search import skill_search_interns
 router = Router()
 
 
-def enhance_search_with_regex(skill_query: str, username: str) -> list:
-    try:
-        pattern = re.compile(rf"\b{re.escape(skill_query)}\b", flags=re.IGNORECASE)
-        results = skill_search_interns(skill_query, username)
-        return [item for item in results if pattern.search(item)]
-    except Exception:
-        return skill_search_interns(skill_query, username)
-
-
 @router.message(Form.skill_search)
 async def skill_search(message: Message, state: FSMContext):
     if message.text == 'Меню команд':
         await message.answer('Выберите команду:',
                              reply_markup=admin_keyboard)
     else:
-        interns_skills = enhance_search_with_regex(message.text, message.from_user.username)
+        interns_skills = skill_search_interns(message.text, message.from_user.username)
         text = '\n\n'.join(interns_skills) if interns_skills else 'Стажеры с запрашиваемым скиллом не найдены'
         await message.answer(text, reply_markup=admin_keyboard)
     await state.set_state(Form.main_admin)
